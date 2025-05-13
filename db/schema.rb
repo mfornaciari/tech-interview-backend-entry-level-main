77a49c280de72f781c3179bcbac1226d6027a7b6
Marco Fornaciari <mfornaciari@gmail.com>
2025-05-13 14:07:06 -0300

    chore: add last_interaction_at column to carts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_192736) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_13_170139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_192736) do
     t.decimal "total_price", precision: 17, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_interaction_at", default: "2025-05-13 17:03:44", null: false
   end
 
   create_table "products", force: :cascade do |t|
--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :shopping_cart, class: 'Cart' do
     total_price { 100.00 }
+    last_interaction_at { Time.current }
   end
 end
