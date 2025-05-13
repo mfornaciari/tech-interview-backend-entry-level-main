e7c4bd65211fec87f581610a520f2fac248f7fd4
Marco Fornaciari <mfornaciari@gmail.com>
2025-05-13 14:22:06 -0300

    feat: add /cart and /cart/add_item routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  resource :cart, only: %i[show] do
+    post 'add_item'
+  end
   resources :products
   get "up" => "rails/health#show", as: :rails_health_check
 
