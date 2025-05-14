e6a73a2066f47b8c8f8a5691b9b0ffcf44c12aee
Marco Fornaciari <mfornaciari@gmail.com>
2025-05-14 14:49:16 -0300

    feat: add DELETE cart/:product_id route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resource :cart, only: %i[show create] do
     post 'add_item'
+    delete ':product_id', to: 'carts#destroy'
   end
   resources :products
   get "up" => "rails/health#show", as: :rails_health_check
