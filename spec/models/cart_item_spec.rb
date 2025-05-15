3d8be0029c78a80432eac1d379bb0095e7a62d73
Marco Fornaciari <mfornaciari@gmail.com>
2025-05-15 15:09:39 -0300

    test: test CartItem#total_price

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 RSpec.describe CartItem, type: :model do
   it { is_expected.to validate_numericality_of(:quantity).is_greater_than(0) }
+
+  describe '#total_price' do
+    subject(:total_price) { cart_item.total_price }
+
+    let(:cart_item) { create(:cart_item, cart: cart, product: product, quantity: 2)}
+    let(:cart) { create(:shopping_cart) }
+    let(:product) { create(:product, price: 10.0)}
+
+    it { is_expected.to eq(20.0) }
+  end
 end
