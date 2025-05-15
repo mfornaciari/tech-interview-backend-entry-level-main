9ed80f5a0d5cc852e6129ef2903fed193e944412
Marco Fornaciari <mfornaciari@gmail.com>
2025-05-15 09:41:16 -0300

    fix: correct Sidekiq config to run jobs from Web UI

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,3 @@
-if defined?(Sidekiq::Scheduler) && !Rails.env.test?
-  Sidekiq.configure_client do |config|
-    schedule_file = Rails.root.join('config/sidekiq.yml')
-    schedule = YAML.load_file(schedule_file).dig(':scheduler', ':schedule')
-    Sidekiq.schedule = schedule
-  end
-end
+schedule_file = Rails.root.join('config/sidekiq.yml')
+schedule = YAML.load_file(schedule_file)[:scheduler][:schedule]
+Sidekiq.schedule = schedule
